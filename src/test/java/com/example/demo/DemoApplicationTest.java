ae9043691a33689668aef83a977e29ac158f97a0
A1bhinish <abhinish@prodevans.com>
2018-08-30 14:35:58 +0530

    Hello-world created

---
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DemoApplicationTest {
+
+    @Test
+    public void home() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
